3337fca81486d43987219fa60a1d09ac1fa3cdf0
DjamelBen <33724263+Djamel02@users.noreply.github.com>
2022-04-09 02:52:49 +0100

    finish app initialization

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
 
 func main()  {
-	fmt.Println("Hello")
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp",":8888")
+	if err != nil {
+		log.Fatalf("Cannot start server on %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("Server started on %s", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("cannot accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
 }
